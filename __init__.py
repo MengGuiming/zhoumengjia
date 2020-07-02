ab4524760942581e215bfac99a20c21df40179a2
Mengjia Zhou <mengjia_zhou@sina.com>
2020-07-02 22:44:05 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# from .ConfigBiLSTM import ConfigBiLSTM
-# from .ConfigBertGcn import ConfigBertGcn
-# from .ConfigXLNetEVI import ConfigXLNetEVI
-# from .ConfigBertEvi import ConfigBertEvi
-# from .ConfigBertMTL import ConfigBertMTL
-from .ConfigBertMTL1 import ConfigBertMTL1
-# from .ConfigBertEviOld import ConfigBertEviOld
-# from .EviConfig import EviConfig
